92258e1371afc414a5ecbc616520521d2da91eb4
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2016-09-06 23:15:50 +0100

    Initial commit

---
+++ components/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+import styles from '../styles/styles'
+
+class App extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>I Want</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
