3dc14fad2bb55a8639aebc3aa2ff42acfb75a724
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2016-09-07 09:23:51 +0100

    Fields added to form

--- components/NewItem.js
+++ components/NewItem.js
@@ -15,7 +15,10 @@ const NewItem = React.createClass({
   render() {
     return (
       <GiftedForm
-        formName='locationForm'
+        formName='itemForm'
+        openModal={(route) => {
+          navigator.push(route); // The ModalWidget will be opened using this method. Tested with ExNavigator
+        }}
         validators={{
           caption: {
             title: 'Caption',
@@ -29,21 +32,21 @@ const NewItem = React.createClass({
 
         defaults={{
           caption: ''
-        }}
-      >
-      <GiftedForm.TextInputWidget
-        name='caption'
-        autoComplete={false}
-        autoCorrect={false}
-        autoCapitalize="sentences"
-        spellcheck={false}
-        title='Caption'
-        clearButtonMode='while-editing'
-        autoFocus={true}
-      />
+        }}>
+
+        <GiftedForm.TextInputWidget
+          name='caption'
+          autoComplete={false}
+          autoCorrect={false}
+          autoCapitalize="sentences"
+          spellcheck={false}
+          title='Caption'
+          clearButtonMode='while-editing'
+          autoFocus={true}
+        />
 
 
-    <GiftedForm.SeparatorWidget/>
+      <GiftedForm.SeparatorWidget/>
 
       <GiftedForm.TextAreaWidget
           autoComplete={false}
@@ -52,30 +55,64 @@ const NewItem = React.createClass({
           spellcheck={false}
           name='comment'
           title='Comment'
-          placeholder='Some reminder'
+          placeholder='Some details'
           clearButtonMode='while-editing'
         />
 
-      <GiftedForm.SubmitWidget
-        title='Save'
-        widgetStyles={{
-          submitButton: {
-            backgroundColor: 'orange',
-          }
-        }}
-        onSubmit={(isValid, values, validationResults, postSubmit = null, modalNavigator = null) => {
-          if (isValid === true) {
-            console.log('Valid');
-            postSubmit();
-            GiftedFormManager.reset('locationForm');
-            this.saveFormData(values.caption, values.comment)
-          } else {
-            console.log('Not valid');
-          }
-        }}
-      />
+        <GiftedForm.SeparatorWidget/>
 
-    </GiftedForm>
+        <GiftedForm.SelectWidget name='gender' title='Type' multiple={false}>
+        <GiftedForm.OptionWidget title='Allowance' value='Allowance'/>
+          <GiftedForm.OptionWidget title='Object' value='Object'/>
+          <GiftedForm.OptionWidget title='More Money' value='More Money'/>
+          <GiftedForm.OptionWidget title='Activity' value='Activity'/>
+          <GiftedForm.OptionWidget title='Edible' value='Edible'/>
+          <GiftedForm.OptionWidget title='Travel' value='Travel'/>
+          <GiftedForm.OptionWidget title='Other' value='Other'/>
+        </GiftedForm.SelectWidget>
+
+        <GiftedForm.SeparatorWidget />
+
+        <GiftedForm.DatePickerIOSWidget
+          name='birthday'
+          mode='date'
+
+          getDefaultDate={() => {
+            return new Date(((new Date()).getFullYear() - 18)+'');
+          }}
+        />
+
+        <GiftedForm.SeparatorWidget/>
+
+        <GiftedForm.TextInputWidget
+          name='cost'
+          keyboardType='number-pad'
+          title='Cost'
+          clearButtonMode='while-editing'
+          autoFocus={true}
+        />
+
+        <GiftedForm.SeparatorWidget/>
+
+      <GiftedForm.SubmitWidget
+          title='Save'
+          widgetStyles={{
+            submitButton: {
+              backgroundColor: 'orange',
+            }
+          }}
+          onSubmit={(isValid, values, validationResults, postSubmit = null, modalNavigator = null) => {
+            if (isValid === true) {
+              console.log('Valid');
+              postSubmit();
+              GiftedFormManager.reset('locationForm');
+              this.saveFormData(values.caption, values.comment)
+            } else {
+              console.log('Not valid');
+            }
+          }}
+        />
+      </GiftedForm>
     )
   }
 })
--- helpers/realm.js
+++ helpers/realm.js
@@ -1,2 +1,17 @@
 import Realm from 'realm';
 import Moment from 'moment';
+
+let realm = new Realm({
+  schema: [{
+    name: 'Items',
+    properties: {
+      caption: 'string',
+      comment: 'string',
+      boughtDate: 'date',
+      cost: 'float',
+      itemType: 'string',
+      status: 'string',
+      soldDate: 'date',
+      soldPrice: 'float'
+    }}]
+});
