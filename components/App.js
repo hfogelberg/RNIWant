233d5ba701db760cbec16df9685a636ad043b01d
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2016-09-09 14:24:10 +0100

    Removed drawer. Will used tabbed navigation instead

--- components/App.js
+++ components/App.js
@@ -1,71 +1,33 @@
 import React, {Component} from 'react';
 import {
-  DeviceEventEmitter,
   View,
   Text,
-  TouchableOpacity,
   Navigator,
   StyleSheet
 } from 'react-native';
-import Drawer from 'react-native-drawer';
 import {EventEmitter} from 'fbemitter';
-import navigationHelper from '../helpers/navigationHelper';
 import styles from '../styles/navigation';
 import NewItem from './NewItem';
 import Items from './Items';
-import Menu from './Menu';
-
-let _emitter = new EventEmitter();
 
 class App extends Component{
-  componentDidMount() {
-    var self = this;
-
-    // Don't show warnings in the simulator
-    // console.disableYellowBox = true;
-
-    _emitter.addListener('openMenu', () => {
-        self._drawer.open();
-    });
-
-    _emitter.addListener('back', () => {
-        self._navigator.pop();
-    });
-  }
-
   render() {
     return (
-      <Drawer
-          ref={(ref) => this._drawer = ref}
-          type="overlay"
-          content={<Menu navigate={(route) => {
-              this._navigator.push(navigationHelper(route));
-              this._drawer.close()
-          }}/>}
-          tapToClose={true}
-          openDrawerOffset={0.2}
-          panCloseMask={0.2}
-          closedDrawerOffset={-3}
-          styles= {styles.navbar}
-          tweenHandler={(ratio) => ({
-              main: { opacity:(2-ratio)/2 }
-          })}>
-            <Navigator
-                ref={(ref) => this._navigator = ref}
-                configureScene={(route) => Navigator.SceneConfigs.FloatFromLeft}
-                initialRoute={{
-                    id: 'Items',
-                    title: 'Items',
-                    index: 0
-                }}
-                renderScene={(route, navigator) => this._renderScene(route, navigator)}
-                navigationBar={
-                    <Navigator.NavigationBar
-                        style={styles.navBar}
-                        routeMapper={NavigationBarRouteMapper} />
-                }
-            />
-        </Drawer>
+      <Navigator
+          ref={(ref) => this._navigator = ref}
+          configureScene={(route) => Navigator.SceneConfigs.FloatFromLeft}
+          initialRoute={{
+              id: 'NewItem',
+              title: 'NewItem',
+              index: 0
+          }}
+          renderScene={(route, navigator) => this._renderScene(route, navigator)}
+          navigationBar={
+              <Navigator.NavigationBar
+                  style={styles.navBar}
+                  routeMapper={NavigationBarRouteMapper} />
+          }
+      />
     );
   }
 
@@ -81,28 +43,11 @@ class App extends Component{
 
   const NavigationBarRouteMapper = {
     LeftButton(route, navigator, index, navState) {
-      switch (route.id) {
-        case 'NewItem':
-          return (
-              <TouchableOpacity
-                  style={styles.navBarLeftButton}
-                  onPress={() => {navigator.pop()}}>
-                  <Text>Back</Text>
-              </TouchableOpacity>
-          )
-        default:
-          return (
-            <TouchableOpacity
-              style={styles.navBarLeftButton}
-              onPress={() => {_emitter.emit('openMenu')}}>
-              <Text>Menu</Text>
-            </TouchableOpacity>
-          )
-      }
+      return null;
     },
 
   RightButton(route, navigator, index, navState) {
-        return null;
+    return null;
   },
 
   Title(route, navigator, index, navState) {
